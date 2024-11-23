d894c1cd46f78719a86470c444e525e9c02f6cbb
Gytis Valukonis <gytis.valukonis@gmail.com>
2024-11-23 22:34:11 +0200

    wezterm config updated for keybindings

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,13 +2,44 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.enable_wayland = false
-
 config.color_scheme = "nord"
 config.font = wezterm.font("GeistMono Nerd Font Mono")
 
 config.keys = {
 	{ key = "Enter", mods = "ALT", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "c", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "v", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "m", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "n", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "-", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "-", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "=", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "=", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "0", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "0", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "t", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "T", mods = "SUPER|SHIFT", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "w", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "0", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "1", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "2", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "3", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "4", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "5", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "6", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "7", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "8", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "9", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "PageUp", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "PageDown", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "PageUp", mods = "SHIFT", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "PageDown", mods = "SHIFT", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "r", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "h", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "k", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "f", mods = "SUPER", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "{", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "}", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(1) },
 }
 
 return config
