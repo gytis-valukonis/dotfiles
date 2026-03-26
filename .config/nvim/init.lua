0b591ee4119b26822fa80ff3c0ddad1466e7d603
Gytis Valukonis <gytis.valukonis@vinted.com>
2026-03-26 13:54:11 +0200

    comp specific servers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -480,25 +480,12 @@ require("lazy").setup({
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-			local servers = {
-				pyright = {},
-				ruby_lsp = {
-					root_dir = function(fname)
-						return require("lspconfig.util").root_pattern("Gemfile", ".ruby-version", "Rakefile", ".git")(fname)
-							or vim.fn.getcwd()
-					end,
-				},
-
-				lua_ls = {
-					settings = {
-						Lua = {
-							completion = {
-								callSnippet = "Replace",
-							},
-						},
-					},
-				},
-			}
+			local servers = {}
+			-- Load machine-local LSP servers from ~/.nvim-servers.lua (not tracked in dotfiles)
+			local ok, local_servers = pcall(dofile, vim.fn.expand("~/.nvim-servers.lua"))
+			if ok and type(local_servers) == "table" then
+				servers = local_servers
+			end
 
 			require("mason").setup()
 
